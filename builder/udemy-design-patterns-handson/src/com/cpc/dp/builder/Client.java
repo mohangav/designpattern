5caa60378c6b3681667fa1c365898ffc22b71fc7
mohan <mohangavande@gmail.com>
2020-08-01 19:09:46 +0530

    Builder 1

--- builder/udemy-design-patterns-handson/src/com/cpc/dp/builder/Client.java
+++ builder/udemy-design-patterns-handson/src/com/cpc/dp/builder/Client.java
@@ -6,7 +6,20 @@ import java.time.LocalDate;
 public class Client {
 
 	public static void main(String[] args) {
+		System.out.println("Hellloooeee");
+		User user = createUser();
+		UserDTOBuilder builder = new UserWebDTOBuilder();
+		UserDTO dto = directBuild(builder, user);
+		System.out.println(dto);
+	}
+	
+	public static UserDTO directBuild(UserDTOBuilder builder, User user) {
 		
+		return builder.withFirstName(user.getFirstName())
+			.withLastName(user.getLastName())
+			.withAddress(user.getAddress())
+			.withBirthday(user.getBirthday())
+			.build();
 	}
 	
 	/**
